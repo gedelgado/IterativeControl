87a3a7dda70238217223c2f1c74ba1261b448508
gedelgado <gen.delvar@gmail.com>
2021-06-17 14:42:46 -0600

    IterativeControl

---
+++ src/uh/ac/cr/Main.java
@@ -0,0 +1,24 @@
+package uh.ac.cr;
+import uh.ac.cr.iterativeControl.IterativeControl;
+
+public class Main {
+
+    public static void main(String[] args) {
+        IterativeControl iterativeControl=new IterativeControl();
+        iterativeControl.check();
+
+        //TODO- Create a new class and call it IterativeControl. ready
+
+        //Do the next operation on the new class created.
+
+        //1- Todo. Ask user how many time he wants to print hello world on screen.
+            //1.1- Todo. Print Hello world as many times as user defined. Use a while structure.
+            //1.2- Todo. Print Hello world as many times as user defined. Use a for structure.
+
+        //2- Todo Print hello world and ask the user if he wants to print hello again using a Do while structure.
+
+        //3- Todo ask the user if he wants to print hello using a while structure to keep printing until user wants to stop.
+
+        //Do all the code needed for the previous items on the class created. Use main just to call the methods.
+    }
+}
---
+++ src/uh/ac/cr/iterativeControl/IterativeControl.java
@@ -0,0 +1,54 @@
+package uh.ac.cr.iterativeControl;
+
+import java.util.Scanner;
+
+public class IterativeControl {
+    Scanner scanner;
+    int option;
+
+    public IterativeControl(){
+        option=0;
+        scanner=new Scanner(System.in);
+    }
+    public void check(){
+        do {
+            System.out.println("¿How many times do you want to print Hello world?");
+            int amount = scanner.nextInt();
+            System.out.println("which option do you want to select" +"\n" +"1-Bucle while" + "\n" + "2-Bucle for" + "\n" + "3-bucle do while"+ "\n"+"4-close");
+            option = (int) scanner.nextFloat();
+            int cont = 0;
+
+            switch (option) {
+                case 1:
+
+                    while (cont<amount) {
+                        System.out.println("HELLO WORLD");
+                        cont++;
+                    }
+                    break;
+                case 2:
+                    System.out.println("2-Bucle for");
+                    for (cont = 0; cont < amount; cont++){
+                        System.out.println("HELLO WORLD");
+                    }
+                    break;
+                case 3:
+                    do {
+                        System.out.println("Hello world");
+                        cont++;
+                    }while (cont<amount);
+                    break;
+
+                    case 4: // finish program
+                    System.out.println("Program finished");
+                    break;
+                default:
+                    System.out.println("Error, ingrese un valor dentro del rango 1 a 4");
+                    break;
+                }
+
+            }while (option<4);
+
+        }
+    }
+
